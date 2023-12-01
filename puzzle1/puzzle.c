1026ba04cfe61c2211d59b3034fa1464e9766b68
Your Name <you@example.com>
2023-12-01 06:22:05 -0500

    started developing puzzle1

---
+++ puzzle1/puzzle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <test.h>
+
+int main(){
+
+    printf("Testadsf: %d\n", testadd(5,7));
+
+    return 0;
+
+}
---
+++ puzzle1/test.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int testadd(int a, int b);

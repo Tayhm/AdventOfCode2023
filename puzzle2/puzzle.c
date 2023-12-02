d024c965aa17b67d0c4a424dade2c40f4603b25a
Timm Weber <s211273@student.dhbw-mannheim.de>
2023-12-02 11:40:37 -0500

    implemented part 2 of puzzle 2

--- puzzle2/puzzle.c
+++ puzzle2/puzzle.c
@@ -13,7 +13,7 @@ int main(){
     int max_red = 12;
     int max_green = 13;
     int max_blue = 14;
-    int id, sum, red, green, blue, num_current = 0;
+    int id = 0, part1_sum = 0, part2_sum = 0, red = 0, green = 0, blue = 0, num_current = 0, min_red = 0, min_green = 0, min_blue = 0;
 
     input = fopen("input", "r");
 
@@ -27,6 +27,10 @@ int main(){
         
         id++;
         is_possible = 1;
+        min_red = 0;
+        min_green = 0;
+        min_blue = 0;
+
 
         //First skip to the actual data, don't care about "game" etc:
         current = strstr(line, ":");
@@ -46,16 +50,19 @@ int main(){
                     if(num_current > max_red){
                         is_possible = 0;
                     }
+                    min_red = num_current > min_red ? num_current : min_red;
                     break;
                 case (int)'g':
                     if(num_current > max_green){
                         is_possible = 0;
                     }
+                    min_green = num_current > min_green ? num_current : min_green;
                     break;
                 case (int)'b':
                     if(num_current > max_blue){
                         is_possible = 0;
                     }
+                    min_blue = num_current > min_blue ? num_current : min_blue;
                     break;
 
             }
@@ -66,12 +73,15 @@ int main(){
         }
 
         if(is_possible){
-            sum += id;
+            part1_sum += id;
         }
+
+        part2_sum += min_blue * min_green * min_red;
 
     }
 
-    printf("Result = %d\n", sum);
+    printf("Result of part 1: %d\n", part1_sum);
+    printf("Result of part 2: %d\n", part2_sum);
 
     return 0;
 
